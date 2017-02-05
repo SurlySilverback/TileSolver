3ee9350e663d630ba72749c3e19f36f83d0cc542
SurlySilverback <SurlySilverback@github.com>
2017-02-05 15:09:44 -0800

    first commit

---
+++ Tile.cpp
@@ -0,0 +1,14 @@
+#ifndef TILE_CPP
+#define TILE_CPP
+
+Tile(int number)
+{
+    myNumber = number;
+}
+
+int Number()
+{
+    return myNumber;
+}
+
+#endif
---
+++ Tile.h
@@ -0,0 +1,17 @@
+#ifndef TILE_H
+#define TILE_H
+
+#include <iostream>
+
+class Tile
+{
+    public:
+	Tile(int number);
+	int Number();
+
+    private:
+        int myNumber;
+};
+
+
+#endif
